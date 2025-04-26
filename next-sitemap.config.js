80fde7a0091ba598b67802ab69372d82d3df0cba
Jairo Erazo <92348509+LilianaAguilar14@users.noreply.github.com>
2025-04-26 00:59:32 -0600

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://jcsafety-surfacing.com', // Tu dominio
+    generateRobotsTxt: true, // Genera también el robots.txt
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 7000,
+  };
+  
